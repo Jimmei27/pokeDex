e9be360fa611206529ec303063326abd6c848e43
J Mei <Jmei@JMei-MacBook-Pro-7.local>
2020-07-13 23:49:49 -0400

    Updates

---
+++ precache-manifest.3486c597abaf97d5a8f932808db0deb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a380c7624370d1a18b1190df0014a5",
+    "url": "/Jimmei27/pokeDex/index.html"
+  },
+  {
+    "revision": "ebf3663945e7d6041d5b",
+    "url": "/Jimmei27/pokeDex/static/css/main.4e7bb2fa.chunk.css"
+  },
+  {
+    "revision": "4d31fb586810e79de87f",
+    "url": "/Jimmei27/pokeDex/static/js/2.07f6c987.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jimmei27/pokeDex/static/js/2.07f6c987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf3663945e7d6041d5b",
+    "url": "/Jimmei27/pokeDex/static/js/main.b9ca0e4a.chunk.js"
+  },
+  {
+    "revision": "d98d54be72a66af72052",
+    "url": "/Jimmei27/pokeDex/static/js/runtime-main.faca8e8d.js"
+  },
+  {
+    "revision": "f5349d9bb509db4760dac6a1864c5455",
+    "url": "/Jimmei27/pokeDex/static/media/pokemonWallpaper.f5349d9b.jpg"
+  }
+]);
